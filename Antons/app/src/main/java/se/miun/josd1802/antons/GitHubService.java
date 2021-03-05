6f97f41444541b1c65d2278411187242aae3a407
OsmelLavalier <you@example.com>
2021-03-05 16:34:59 +0100

    update

--- Antons/app/src/main/java/se/miun/josd1802/antons/GitHubService.java
+++ Antons/app/src/main/java/se/miun/josd1802/antons/GitHubService.java
@@ -1,4 +1,13 @@
 package se.miun.josd1802.antons;
 
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface GitHubService {
+    @GET("users/{user}/repos")
+    Call<List<Repo>> listRepos(@Path("user") String user);
 }
--- Antons/app/src/main/java/se/miun/josd1802/antons/Repo.java
+++ Antons/app/src/main/java/se/miun/josd1802/antons/Repo.java
@@ -1,4 +1,14 @@
 package se.miun.josd1802.antons;
 
 public class Repo {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
 }
