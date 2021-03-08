5d6028802cf51ab54b89ac0b2f76d445d4b4e616
shsh1801 <70692541+shsh1801@users.noreply.github.com>
2021-03-08 09:57:51 +0100

    schemasida test

--- Antons/app/src/main/java/se/miun/josd1802/antons/Order.java
+++ Antons/app/src/main/java/se/miun/josd1802/antons/Order.java
@@ -5,10 +5,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Order extends AppCompatActivity {
+public class Order extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Spinner spinner1 = findViewById(R.id.spinner1);
+        Spinner spinner2 = findViewById(R.id.spinner2);
+        Spinner spinner3 = findViewById(R.id.spinner3);
+        Spinner spinner4 = findViewById(R.id.spinner4);
+        Spinner spinner5 = findViewById(R.id.spinner5);
+        Spinner spinner6 = findViewById(R.id.spinner6);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.numbers, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter);
+        spinner1.setOnItemSelectedListener(this);
+        spinner2.setAdapter(adapter);
+        spinner2.setOnItemSelectedListener(this);
+        spinner3.setAdapter(adapter);
+        spinner3.setOnItemSelectedListener(this);
+        spinner4.setAdapter(adapter);
+        spinner4.setOnItemSelectedListener(this);
+        spinner5.setAdapter(adapter);
+        spinner5.setOnItemSelectedListener(this);
+        spinner6.setAdapter(adapter);
+        spinner6.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
 
   /*  private Button b_oversikt;
     private EditText e_måltid1;
--- Antons/app/src/main/java/se/miun/josd1802/antons/Schedule.java
+++ Antons/app/src/main/java/se/miun/josd1802/antons/Schedule.java
@@ -2,13 +2,125 @@ package se.miun.josd1802.antons;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.security.PrivateKey;
 
 public class Schedule extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        setupUIViews();
+        initToolbar();
+        
     }
+
+    private void setupUIViews() {
+        toolbar = (Toolbar)findViewById(R.id.ToolbarSchedule);
+        listView = (ListView)findViewById(R.id.lvSchedule);
+    }
+
+    private void initToolbar() {
+       setSupportActionBar(toolbar);
+       getSupportActionBar().setTitle("Timetable App");
+       
+    }
+
+    private void setupListView(){
+        String[] title = getResources().getStringArray(R.array.Week);
+        String[] description = getResources().getStringArray(R.array.Description);
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, title, description);
+        listView.setAdapter(simpleAdapter);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+    public class SimpleAdapter extends BaseAdapter{
+
+        private Context sContext;
+        private LayoutInflater layoutInflater;
+        private TextView title, description;
+        private String[] titleArray;
+        private String [] descriptionArray;
+        private ImageView imageView;
+
+        public SimpleAdapter(Context context, String[] title, String[] description){
+            sContext = context;
+            titleArray = title;
+            descriptionArray = description;
+            layoutInflater = LayoutInflater.from(context);
+
+        }
+
+        @Override
+        public int getCount() {
+            return titleArray.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return titleArray[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null) {
+                convertView = layoutInflater.inflate(R.layout.schedule_singel_item, null);
+            }
+
+            title = (TextView)convertView.findViewById(R.id.tvSchedule);
+            description = (TextView)convertView.findViewById(R.id.tvDescription);
+            imageView= (ImageView)convertView.findViewById(R.id.ivSchedule);
+            title.setText(titleArray[position]);
+            description.setText(descriptionArray[position]);
+
+            if(titleArray[position].equalsIgnoreCase("Monday")){
+                imageView.setImageResource(R.drawable.m);
+            }
+            else if (titleArray[position].equalsIgnoreCase("Tuesday")){
+                imageView.setImageResource(R.drawable.t);
+            }
+            else if (titleArray[position].equalsIgnoreCase("Wednesday")){
+                imageView.setImageResource(R.drawable.w);
+            }
+            else if (titleArray[position].equalsIgnoreCase("Thursday")){
+                imageView.setImageResource(R.drawable.t);
+            }
+            else if (titleArray[position].equalsIgnoreCase("Friday")){
+                imageView.setImageResource(R.drawable.f);
+            }
+            else{
+                imageView.setImageResource(R.drawable.s);
+            }
+// (titleArray[position].equalsIgnoreCase("Suturday"))
+
+            return convertView;
+
+
+        }
+    }
+
 }
