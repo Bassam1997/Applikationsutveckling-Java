2cde249a62e8533c53f51c13ffe3ddce7f864289
Bassam1997 <55024807+Bassam1997@users.noreply.github.com>
2021-03-16 20:32:30 +0100

    Uppdaterad hemsida
    Småfix
    
    Co-Authored-By: mahe1711 <77738424+mahe1711@users.noreply.github.com>

--- Antons SkafferiNy1/build/generated-sources/ap-source-output/entities/Dricka_.java
+++ Antons SkafferiNy1/build/generated-sources/ap-source-output/entities/Dricka_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-03-16T20:16:24")
+@Generated(value="EclipseLink-2.7.7.v20200504-rNA", date="2021-03-16T20:30:00")
 @StaticMetamodel(Dricka.class)
 public class Dricka_ { 
 
